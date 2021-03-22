cb8a42445517d1011a9aff0786ba94b9ff07620a
Tabatha Macedo <tabmacedo@gmail.com>
2021-03-21 23:20:07 -0300

    🚚 🚧 added a temporary route and logic of operation

--- server/app.py
+++ server/app.py
@@ -12,11 +12,30 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+#array de resultados prévios
+results= []
 
 # sanity check route
-@app.route('/theory', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+# @app.route('/theory', methods=['GET'])
+# def ping_pong():
+#     return jsonify('pong!')
+
+@app.route('/theory', methods=['GET']) ## mudara pra results
+def last_results():
+    if not results:
+        response_obj = {'msg': 'Sem resultados no momento.'}
+    else:
+        response_obj = results[-1]    
+    return jsonify(response_obj)
+
+
+# @app.route('/thory', methods=['POST'])
+# def pythagorean_theorem():
+#     a = float(input)
+#     b = float(input)
+
+# c = sqrt(a**2 + b**2)
+
 
 
 if __name__ == '__main__':
